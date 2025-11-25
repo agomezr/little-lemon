c75181251ef45e6d593cde927d5e6734eabeb2e8
Alberto Gómez <alberto@masweb.es>
2025-11-25 01:25:10 +0100

    Ready for gh-pages test

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  /* For gh-pages */
+  base: '/<nombre-de-tu-repositorio>/',
   plugins: [
     react(),
     tailwindcss(),
